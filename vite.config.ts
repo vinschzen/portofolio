9a03a197b71b7e708010cd978e44756cca25028a
vinschzen <vinshentwianata@gmail.com>
2025-12-05 18:51:23 +0700

    link

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio",
+  base: "https://vinschzen.github.io/portofolio",
   plugins: [
     react({
       babel: {
